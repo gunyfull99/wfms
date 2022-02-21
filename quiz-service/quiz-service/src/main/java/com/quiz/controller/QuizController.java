f7d73a240ed92485979b987db639e9c7da8f7b36
hieu <hieu000502@gmail.com>
2022-02-21 10:54:58 +0700

    z

--- quiz-service/quiz-service/src/main/java/com/quiz/controller/QuizController.java
+++ quiz-service/quiz-service/src/main/java/com/quiz/controller/QuizController.java
@@ -18,7 +18,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.List;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
--- quiz-service/quiz-service/src/main/java/com/quiz/service/NomineeService.java
+++ quiz-service/quiz-service/src/main/java/com/quiz/service/NomineeService.java
@@ -3,13 +3,47 @@ package com.quiz.service;
 import com.quiz.Dto.NomineeEditRequest;
 import com.quiz.Dto.NomineeRequest;
 import com.quiz.entity.Nominee;
+import com.quiz.repository.NomineeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public interface NomineeService {
-    void createNominee(NomineeRequest nomineeRequest);
+@Service
+public class NomineeService  {
 
-    void editNominee(NomineeEditRequest nomineeRequest);
-     List<Nominee> getAll();
-    void deleteNominee(Long id);
+    @Autowired
+    NomineeRepository nomineeRepository;
+
+    private static final Logger logger = LoggerFactory.getLogger(NomineeService.class);
+
+
+    public void createNominee(NomineeRequest nomineeRequest) {
+        Nominee nomineeEntity = new Nominee();
+        nomineeEntity.setName(nomineeRequest.getName());
+        nomineeEntity.setStatus(nomineeRequest.isStatus());
+        nomineeRepository.save(nomineeEntity);
+    }
+
+    public List<Nominee> getAll() {
+        logger.info("get all nominee");
+        return nomineeRepository.findAll();
+    }
+
+    public void editNominee(NomineeEditRequest nomineeRequest) {
+        logger.info("Receive infor of nominee {} to edit", nomineeRequest.getName());
+        Nominee nomineeEntity = nomineeRepository.getById(nomineeRequest.getId());
+        nomineeEntity.setName(nomineeRequest.getName());
+        nomineeEntity.setStatus(nomineeRequest.isStatus());
+        nomineeRepository.save(nomineeEntity);
+    }
+
+    public void deleteNominee(Long id) {
+        logger.info("Receive id of nominee {} to delete", id);
+
+        Nominee nomineeEntity = nomineeRepository.getById(id);
+        nomineeEntity.setStatus(false);
+    }
 }
