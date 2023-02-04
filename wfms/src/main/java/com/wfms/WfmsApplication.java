e360c72f894f9e16b036549b0518455ca619bb03
Nguyễn Minh Hiếu <hieu000502@gmail.com>
2023-02-04 20:41:12 +0700

    aa

---
+++ wfms/src/main/java/com/wfms/WfmsApplication.java
@@ -0,0 +1,17 @@
+package com.wfms;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication
+(exclude = {SecurityAutoConfiguration.class},scanBasePackages={
+"com.wfms"})
+public class WfmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WfmsApplication.class, args);
+    }
+
+}
---
+++ wfms/src/main/java/com/wfms/controller/Controller.java
@@ -0,0 +1,17 @@
+package com.wfms.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequestMapping("/")
+public class Controller {
+    @GetMapping("a")
+    public String getDetailUser()  {
+
+        return "aaa";
+    }
+}
