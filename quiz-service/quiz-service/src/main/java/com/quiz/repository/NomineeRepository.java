961ce8f2ba0835e286eaf9ea1b798348546ffbbe
Nguyễn Minh Hiếu <hieu000502@gmail.com>
2023-02-04 19:53:24 +0700

    z

--- quiz-service/quiz-service/src/main/java/com/quiz/repository/NomineeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.quiz.repository;
-
-import com.quiz.entity.Nominee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NomineeRepository extends JpaRepository<Nominee,Long> {
-}
