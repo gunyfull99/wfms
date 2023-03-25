442aac6074a557f21ab4e764c43af93b35fe841d
trungdv <trungdv@hitexglobal.com>
2023-03-25 21:43:55 +0700

    try update CORS filter

--- wfms/src/main/java/com/wfms/filter/SimpleCORSFilter.java
+++ wfms/src/main/java/com/wfms/filter/SimpleCORSFilter.java
@@ -1,14 +1,20 @@
 package com.wfms.filter;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 
 @Component
-public class SimpleCORSFilter implements Filter {
+public class SimpleCORSFilter  extends WebSecurityConfigurerAdapter implements Filter{
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
     }
@@ -29,5 +35,16 @@ public class SimpleCORSFilter implements Filter {
 
     @Override
     public void destroy() {
+    }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource()
+    {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
